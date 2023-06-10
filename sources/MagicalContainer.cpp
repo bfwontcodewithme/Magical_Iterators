4c738fa0fded274478bcd361361ee928b6e5fe39
bfwontcodewithme <75036857+bfwontcodewithme@users.noreply.github.com>
2023-06-10 13:16:46 +0300

    compile, specific errors

--- sources/MagicalContainer.cpp
+++ sources/MagicalContainer.cpp
@@ -1,16 +1,54 @@
 #include "MagicalContainer.hpp"
+#include <stdexcept>
 namespace ariel{
-    MagicalContainer::MagicalContainer()
-    {
+    bool comPtr(const int* a,const int* b){
+        return *a < *b;
     }
-
     void MagicalContainer::addElement(int element)
     {
+        /**
+         * adding element in order to vector 
+         * finding index of the closest bigger number, return ptr to the index
+         * insert new element in that index
+        */
+        auto pit = std::lower_bound(elements.begin(), elements.end(), element);
+        elements.insert(pit, element);
+        /**
+         * check if element is prime if true-> add to primes vector
+        */
+       if(isPrime(element)){
+            int *ptr_e = new int(element);
+            auto prit = std::lower_bound(primes.begin(), primes.end(), ptr_e, comPtr);
+            primes.insert(prit,ptr_e);
+            //-----------------------------------------------------------> CHECK order of ptr
+       }
     }
+    bool MagicalContainer::isPrime(int num)
+    {
+        if(num <= 1) return false;
+        for(int i = 2; i * i <= num; i++){
+            if(num % i == 0) return false;
+        }
+        return true;
+    }
+
     void MagicalContainer::removeElement(int element)
     {
-    }
-    MagicalContainer::~MagicalContainer()
-    {
+        auto ptr = std::find(elements.begin(), elements.end(), element);
+        /** 
+         * ptr type should be iterator
+         * if ptr equals end of vector -> element not exist 
+         */
+        if(ptr != elements.end()){
+            elements.erase(ptr);
+        }
+        else {
+            throw std::runtime_error("not exist in container");
+        }
+        /**
+         * in case element is also prime -> find and remove from prime vector
+        */
+        auto prim = std::find(primes.begin(), primes.end(), &element);  //--------------------------------> CHECK about & in elements
+        if(prim != primes.end()) primes.erase(prim); //if not exist already throws before
     }
 }
--- sources/MagicalContainer.hpp
+++ sources/MagicalContainer.hpp
@@ -1,82 +1,167 @@
 #ifndef MAGICALCONTAINER_HPP
 #define MAGICALCONTAINER_HPP
 #include <vector>
-
+#include <stdexcept>
+int const ascend =1;
+int const sidecross =2;
+int const prime =3;
+/**
+ * 
+*/
 namespace ariel {
     class MagicalContainer{
+        
         private:
-        std::vector <int> elements;
-        public:
-        MagicalContainer();
-        void addElement(int element );
-        void removeElement(int element);
-        size_t size(){return elements.size();}     //protects from int overflow
-        ~MagicalContainer();
+        std::vector <int> elements;         //vector with elements of container
+        std::vector <int*> primes;          //vector of ptrs to prime values in elements
+        /*--------------------------------------------------class iterator--------------------------------------------------------------------------*/
+        class Iterator{
+            protected:
+                MagicalContainer* container;
+                size_t current_index;
 
-        class AscendingIterator;
-        class SideCrossIterator;
-        class PrimeIterator;                  
-    };
-    class MagicalContainer::AscendingIterator{
-        private:
-            MagicalContainer& container;
-            size_t current_index;
+
+            public:
+                int type;             // 1-ascending, 2-sidecross, 3-prime
+                Iterator(MagicalContainer &curr_container, size_t current = 0, int type = ascend): container(&curr_container), current_index(current), type(type){};
+                Iterator(const Iterator& other) : container(other.container), current_index(other.current_index){};
+                int getType()const{return this->type;}
+                void checkTypes(const Iterator& other) const {
+                    if( this->type != other.getType()){
+                        throw std::runtime_error("different types");
+                    }
+                }
+                void checkContainers(const Iterator& other)const{
+                    if(this->container != other.container){
+                        throw std::runtime_error("different containers");
+                    }
+                }
+                bool operator<(const Iterator& other)const{
+                    checkTypes(other);
+                    checkContainers(other);
+                    return this->current_index < other.current_index;
+                }
+                bool operator>(const Iterator& other)const{
+                    checkTypes(other);
+                    checkContainers(other);
+                    return this->current_index > other.current_index;
+                }
+                bool operator ==(const Iterator& other)const{
+                    checkTypes(other);
+                    checkContainers(other);
+                    return this->current_index == other.current_index;
+                }
+                bool operator !=(const Iterator& other)const{
+                    return !(*this == other);
+                }
+
+        };
+        /*----------------------------------------------end class iterator--------------------------------------------------------------------------*/
         public:
-            AscendingIterator(MagicalContainer& curr_container, size_t current = 0) : container(curr_container), current_index(current) {};
-            AscendingIterator begin(){ return AscendingIterator(container, 0);}
-            AscendingIterator end(){ return AscendingIterator(container, container.size());}
-            
-            int operator*()const{ return container.elements.at(current_index);}
-            bool operator<(const AscendingIterator& other)const{return true;}
-            bool operator>(const AscendingIterator& other)const{return true;}
-            bool operator ==(const AscendingIterator& other)const{return current_index == other.current_index;}
-            bool operator !=(const AscendingIterator& other)const{return !(*this == other);}
+            MagicalContainer() = default;
+            ~MagicalContainer(){
+                for (int* ptr : primes){
+                    delete ptr;
+                }
+            };
+            MagicalContainer(const MagicalContainer &other) = default;
+            void addElement(int element);
+            void removeElement(int element);
+            size_t size(){return elements.size();}     //protects from int overflow
+            bool isPrime(int num);
+
+
+
+            class AscendingIterator;
+            class SideCrossIterator;
+            class PrimeIterator;                  
+    };
+
+    
+    class MagicalContainer::AscendingIterator : public MagicalContainer::Iterator{      //MagicalContainer is already sorted 
+
+        public:
+            AscendingIterator(MagicalContainer &curr_container, size_t current = 0) : Iterator(curr_container, current, ascend) {};
+            AscendingIterator(const AscendingIterator& other) : Iterator(other){}
+            ~AscendingIterator(){};
+            AscendingIterator begin(){return AscendingIterator(*container, 0);}
+            AscendingIterator end(){return AscendingIterator(*container, container->elements.size());}
+            int operator*()const{ return container->elements.at(current_index);}
+            AscendingIterator &operator=(AscendingIterator& other){
+                if(*this != other){
+                    this->current_index = other.current_index;
+                }
+                return *this;
+            }
             AscendingIterator operator++(){
+                if(current_index == *container->elements.end()){
+                    throw std::runtime_error("vector ended");
+                }
                 ++current_index;
                 return *this;
-            }
-            AscendingIterator &operator=(AscendingIterator& other){return *this;}
-
-
+            }  
     };
-    class MagicalContainer::SideCrossIterator{
-        private:
-            MagicalContainer& container;
-            size_t current_index;
-            public:
-            SideCrossIterator(MagicalContainer& curr_container, size_t current = 0) : container(curr_container), current_index(current) {};
-            SideCrossIterator begin(){ return SideCrossIterator(container, 0);}
-            SideCrossIterator end(){ return SideCrossIterator(container, container.size());}
-            int operator*()const{ return container.elements.at(current_index);}
-            bool operator<(SideCrossIterator& other)const{return true;}
-            bool operator>(SideCrossIterator& other)const{return true;}
-            bool operator ==(SideCrossIterator& other)const{return current_index == other.current_index;}
-            bool operator !=(SideCrossIterator& other)const{return !(*this == other);}
+    class MagicalContainer::SideCrossIterator : public MagicalContainer::Iterator{
+
+        public:
+            SideCrossIterator(MagicalContainer &curr_container, size_t current = 0) : Iterator(curr_container, current, sidecross){};
+            ~SideCrossIterator(){};
+            SideCrossIterator(const SideCrossIterator& other) : Iterator(other){};
+            SideCrossIterator begin(){return SideCrossIterator(*container, 0);}
+            SideCrossIterator end(){return SideCrossIterator(*container, container->elements.size());}
+            int operator*()const{
+                /**
+                 * vector is sorted, index is for iterator; position is place in vector
+                 * for every even index we output from begining+; fro every odd index the output is from the end
+                 * 
+                 * i=0 output first element; i=1 output size - 1/2 (=0) -1 (the last element)
+                 * i=2  (i/2 = 1)output second element from begining, i=3 output size - (3/2 = 1) -1 -> size-2 -> second to last/
+                 * */ 
+                auto index = (current_index % 2 == 0) ? (current_index/2) : container->elements.size() - (current_index/2) -1;
+                return container->elements.at(index);
+            }
+            SideCrossIterator &operator=(SideCrossIterator& other){
+                if(*this != other){
+                    this->current_index = other.current_index;
+                }
+                return *this;
+            }
             SideCrossIterator operator++(){
+                if(current_index == *container->elements.end()){
+                        throw std::runtime_error("vector ended");
+                }
                 ++current_index;
                 return *this;
             }
-            SideCrossIterator &operator=(SideCrossIterator& other){return *this;}
     };
-    class MagicalContainer::PrimeIterator{
-        private:
-            MagicalContainer& container;
-            size_t current_index;
-            public:
-            PrimeIterator(MagicalContainer& curr_container, size_t current = 0) : container(curr_container), current_index(current) {};
-            PrimeIterator begin(){ return PrimeIterator(container, 0);}
-            PrimeIterator end(){ return PrimeIterator(container, container.size());}
-            int operator*()const{ return container.elements.at(current_index);}
-            bool operator<(PrimeIterator& other)const{return true;}
-            bool operator>(PrimeIterator& other)const{return true;}
-            bool operator ==(PrimeIterator& other)const{return current_index == other.current_index;}
-            bool operator !=(PrimeIterator& other)const{return !(*this == other);}
+    class MagicalContainer::PrimeIterator : public MagicalContainer::Iterator{
+
+        public:
+            PrimeIterator(MagicalContainer &curr_container, size_t current = 0) : Iterator(curr_container, current, prime) {};
+            ~PrimeIterator(){};
+            PrimeIterator(const PrimeIterator& other): Iterator(other){};
+            PrimeIterator begin(){return PrimeIterator(*container, 0);}
+            PrimeIterator end(){return PrimeIterator(*container, container->primes.size());}
+            int operator*()const{
+                auto ptr = container->primes.at(current_index);
+                int val = *ptr;
+                return val;
+            }
+            PrimeIterator &operator=(PrimeIterator& other){
+                if(*this != other){
+                    this->current_index = other.current_index;
+                }
+                return *this;
+            };
             PrimeIterator operator++(){
+                if(current_index == *container->elements.end()){
+                        throw std::runtime_error("vector ended");
+                }
                 ++current_index;
                 return *this;
             }
-            PrimeIterator &operator=(PrimeIterator& other){return *this;}
     };
 
 }
+
 #endif
